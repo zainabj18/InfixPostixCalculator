6d05ea196782030fc6bada559c1b5558d024ee78
Zainab Jaffar <zkac205@live.rhul.ac.uk>
2022-12-08 15:11:19 +0000

    created an input field for the calculator on the main pane and its corresponding label

--- CS2800_Calculator/src/main/java/uk/ac/rhul/cs2800/MainView.java
+++ CS2800_Calculator/src/main/java/uk/ac/rhul/cs2800/MainView.java
@@ -2,6 +2,7 @@ package uk.ac.rhul.cs2800;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 
 /**
@@ -14,10 +15,17 @@ public class MainView {
 
   /**
    * Private instance of Pane type called mainPane. Private instance of Label type called
-   * calculatorLabel.
+   * calculatorLabel. Private instance of Label type called inputLabel. Private instance of
+   * TextField type called inputField.
    */
   @FXML
   private Label calculatorLabel;
+
+  @FXML
+  private TextField inputField;
+
+  @FXML
+  private Label inputLabel;
 
   @FXML
   private Pane mainPane;
