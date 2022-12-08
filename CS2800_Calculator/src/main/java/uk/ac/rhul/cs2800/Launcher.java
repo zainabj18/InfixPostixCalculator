1e549dfbbba94d7051f09e6a68d15bb3f0b3fa5c
Zainab Jaffar <zkac205@live.rhul.ac.uk>
2022-12-08 13:46:30 +0000

    created the main pane for the calculator by creating the Driver class, the Launcher class and the mainView which all work to create the main pane from scenebulder :

---
+++ CS2800_Calculator/src/main/java/uk/ac/rhul/cs2800/Launcher.java
@@ -0,0 +1,22 @@
+package uk.ac.rhul.cs2800;
+
+/**
+ * creating a class Launcher that launches the main pane of the calculator from the Driver class.
+ * 
+ * @author Zainab
+ *
+ */
+public class Launcher {
+
+  /**
+   * main method that calls the main method from the Driver class.
+   * 
+   * @param args passed as a parameter in the main method of the Driver class to launch the main
+   *        pane.
+   */
+  public static void main(String[] args) {
+    Driver.main(args);
+
+  }
+
+}
---
+++ CS2800_Calculator/src/main/java/uk/ac/rhul/cs2800/MainView.java
@@ -0,0 +1,25 @@
+package uk.ac.rhul.cs2800;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+
+/**
+ * Created a class called MainView which includes the attributes of the main pane of the calculator.
+ * 
+ * @author Zainab
+ *
+ */
+public class MainView {
+
+  /**
+   * Private instance of Pane type called mainPane. Private instance of Label type called
+   * numberLabel.
+   */
+  @FXML
+  private Pane mainPane;
+
+  @FXML
+  private Label numberLabel;
+
+}
