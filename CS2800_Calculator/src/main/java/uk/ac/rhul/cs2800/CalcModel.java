f3506dcf82bbaf1d056ccc9fc1c0d08de60a3832
Zainab Jaffar <zkac205@live.rhul.ac.uk>
2022-12-15 16:55:22 +0000

    created a method called uponPressingCalculate that will toggle between the two radioButtons. So when you press one button, the other one will be 'unpressed'. This method will also call the appropriate evaluate method from the revPolishCalc if revPolishCalc has been pressed through the use of the CalcModel class that i also createdd. This class has a method called evaluate.

---
+++ CS2800_Calculator/src/main/java/uk/ac/rhul/cs2800/CalcModel.java
@@ -0,0 +1,13 @@
+package uk.ac.rhul.cs2800;
+
+
+public class CalcModel {
+
+  public char[] evaluate(String string, boolean b) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+
+
+}
